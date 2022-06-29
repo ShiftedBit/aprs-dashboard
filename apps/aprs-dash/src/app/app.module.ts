529f038a69901fb9d15369a0e5876f4005f1c15f
Markus Hammerschmid <markus.hammerschmid@mows.at>
2022-06-29 23:59:53 +0200

    add angular material

--- apps/aprs-dash/src/app/app.module.ts
+++ apps/aprs-dash/src/app/app.module.ts
@@ -7,12 +7,24 @@ import { HttpClientModule } from '@angular/common/http';
 import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 const config: SocketIoConfig = { url: 'http://localhost:3333', options: { autoConnect: false} };
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [CommonModule, BrowserModule, HttpClientModule, SocketIoModule.forRoot(config), FormsModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(config),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
